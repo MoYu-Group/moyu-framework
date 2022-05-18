1d61cd81a3f96a5967891fc3c63bdcba803a745c
饭饭 <76698242@qq.com>
2022-05-19 01:08:57 +0800

    feat: 新增 moyu-web-autoconfiguers 模块，用于快速集成web工程

---
+++ moyu-autoconfiguers/moyu-web-autoconfiguers/src/main/java/io/github/moyugroup/web/health/conntroller/HeathCheckController.java
@@ -0,0 +1,19 @@
+package io.github.moyugroup.web.health.conntroller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 健康检查操作
+ * <p>
+ * Created by fanfan on 2022/05/19.
+ */
+@RestController
+public class HeathCheckController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "success";
+    }
+
+}
