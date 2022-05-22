104c9db16c2d012425f7df1311ce9479d0b99513
fuhaixin <fuhaixin77@gmail.com>
2022-05-22 17:08:08 +0800

    :bulb: 补全FrameworkEnvironmentPostProcessor的注释

--- moyu-autoconfiguers/moyu-web-autoconfiguers/src/main/java/io/github/moyugroup/web/listener/FrameworkEnvironmentPostProcessor.java
+++ moyu-autoconfiguers/moyu-web-autoconfiguers/src/main/java/io/github/moyugroup/web/listener/FrameworkEnvironmentPostProcessor.java
@@ -1,22 +1,33 @@
 package io.github.moyugroup.web.listener;
 
+import static org.springframework.core.Ordered.HIGHEST_PRECEDENCE;
+
 import io.github.moyugroup.util.PropertyUtil;
+import java.io.IOException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.env.EnvironmentPostProcessor;
 import org.springframework.boot.env.YamlPropertySourceLoader;
 import org.springframework.core.annotation.Order;
+import org.springframework.core.env.AbstractEnvironment;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.PropertySource;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.util.Assert;
 
-import java.io.IOException;
-@Order(-999)
-public class MyEnvironmentPostProcessor implements EnvironmentPostProcessor {
+/**
+ * 实现EnvironmentPostProcessor接口，加载配置文件,同时也为以后动态加载配置文件做准备
+ */
+@Order(HIGHEST_PRECEDENCE)
+public  class FrameworkEnvironmentPostProcessor implements EnvironmentPostProcessor {
 
     private final YamlPropertySourceLoader loader = new YamlPropertySourceLoader();
 
+    /**
+     * 动态加载配置文件
+     * @param environment 配置文件集合的抽象接口 {@link AbstractEnvironment}
+     * @param application SpringApplication实例 {@link SpringApplication}
+     */
     @Override
     public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
         Resource path = new ClassPathResource("config/framework.yml");
@@ -26,6 +37,11 @@ public class MyEnvironmentPostProcessor implements EnvironmentPostProcessor {
         PropertyUtil.updateEnvironment(environment, true);
     }
 
+    /**
+     * 加载yaml配置文件
+     * @param path 文件路径
+     * @return 属性的抽象
+     */
     private PropertySource<?> loadYaml(Resource path) {
         Assert.isTrue(path.exists(), () -> "Resource " + path + " does not exist");
         try {
